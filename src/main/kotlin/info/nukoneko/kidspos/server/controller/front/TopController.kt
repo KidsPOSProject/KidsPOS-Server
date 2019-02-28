1cd690f5339825738149695c9f6cbc06dc6ff0db
atsumi <atsumi@mail.bizen.jp>
2019-02-28 23:09:50 +0900

    feat: support qr settings.

--- src/main/kotlin/info/nukoneko/kidspos/server/controller/front/TopController.kt
+++ src/main/kotlin/info/nukoneko/kidspos/server/controller/front/TopController.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import java.net.InetAddress
 
 
 @Controller
@@ -22,6 +23,9 @@ class TopController {
     @GetMapping
     fun index(model: Model): String {
         model.addAttribute("title", "top")
+        model.addAttribute("running_canonicalHostName", InetAddress.getLocalHost().canonicalHostName)
+        model.addAttribute("running_address", InetAddress.getLocalHost().address.joinToString(","))
+        model.addAttribute("running_host", InetAddress.getLocalHost().hostName)
         model.addAttribute("running_port", environment.getProperty("local.server.port"))
         return "index"
     }
