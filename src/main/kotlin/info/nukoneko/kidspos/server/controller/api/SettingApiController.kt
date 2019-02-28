c8204b641b16e73e52ca972e761ef3b14876f766
atsumi <atsumi@mail.bizen.jp>
2019-03-01 02:14:25 +0900

    feat: add status check api.

--- src/main/kotlin/info/nukoneko/kidspos/server/controller/api/SettingApiController.kt
+++ src/main/kotlin/info/nukoneko/kidspos/server/controller/api/SettingApiController.kt
@@ -1,10 +1,16 @@
 package info.nukoneko.kidspos.server.controller.api
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/setting")
 class SettingApiController {
+    @RequestMapping("status", method = [RequestMethod.GET])
+    fun getStatus(): StatusBean {
+        return StatusBean("OK")
+    }
 
+    class StatusBean(val status: String)
 }
