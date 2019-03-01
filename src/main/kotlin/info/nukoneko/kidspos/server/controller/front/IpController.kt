4beb3fee2d8a2ff61d4308de586a639e9e543e3b
atsumi <atsumi@mail.bizen.jp>
2019-03-01 17:02:50 +0900

    refactor: ip いちらん

--- src/main/kotlin/info/nukoneko/kidspos/server/controller/front/IpController.kt
+++ src/main/kotlin/info/nukoneko/kidspos/server/controller/front/IpController.kt
@@ -7,7 +7,8 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import java.net.InetAddress
-
+import java.net.NetworkInterface
+import java.util.*
 
 @Controller
 @RequestMapping("/ip")
@@ -18,12 +19,28 @@ class IpController {
 
     @GetMapping
     fun index(model: Model): String {
+        val hosts: MutableList<HostBean> = mutableListOf()
+        val testMIP = NetworkInterface.getNetworkInterfaces()
+        while (testMIP.hasMoreElements()) {
+            val testNI = testMIP.nextElement() as NetworkInterface
+            val testInA = testNI.inetAddresses
+            while (testInA.hasMoreElements()) {
+                val testIP = testInA.nextElement() as InetAddress
+                if (testIP.hostName.contains(":0:") || testIP.hostAddress.contains(":0:")) {
+                    continue
+                }
+                hosts.add(HostBean(testIP.hostName, testIP.hostAddress))
+            }
+        }
+
         model.addAttribute("title", "ip")
-        model.addAttribute("canonicalHostName", InetAddress.getLocalHost().canonicalHostName)
-        model.addAttribute("address", InetAddress.getLocalHost().address)
-        model.addAttribute("hostName", InetAddress.getLocalHost().hostName)
-        model.addAttribute("hostAddress", InetAddress.getLocalHost().hostAddress)
-        model.addAttribute("running_port", environment.getProperty("local.server.port"))
+        model.addAttribute("hosts", hosts)
+        model.addAttribute("port", environment.getProperty("local.server.port"))
         return "ip/index"
+    }
+
+    class HostBean(val name: String, val address: String) {
+        val nameId: String = UUID.randomUUID().toString()
+        val addressId: String = UUID.randomUUID().toString()
     }
 }
